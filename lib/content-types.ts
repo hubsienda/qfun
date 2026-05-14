81375c7547108aaea4cdb86a506da1a9748f9dc7
hubsienda <hub@siendaweblines.com>
2026-05-14 15:53:51 +0200

    Update content-types.ts

--- lib/content-types.ts
+++ lib/content-types.ts
@@ -8,8 +8,10 @@ export type HomeContent = {
   heroTitle: string;
   heroLine: string;
   heroDescription: string;
+  heroPurpose: string;
   heroButton: string;
   heroSupport: string;
+
   encounterEyebrow: string;
   encounterTitle: string;
   question: string;
@@ -18,18 +20,42 @@ export type HomeContent = {
   answerC: string;
   answerD: string;
   correctAnswer: string;
+
   correctResponse: string;
+  correctDiagnosis: string;
+  correctDetectedMyth: string;
+  correctRealityCheck: string;
+  correctRecommendedAntidote: string;
+
   wrongResponse: string;
+  wrongDiagnosis: string;
+  wrongDetectedMyth: string;
+  wrongRealityCheck: string;
+  wrongRecommendedAntidote: string;
+
+  diagnosisLabel: string;
+  detectedMythLabel: string;
+  realityCheckLabel: string;
+  recommendedAntidoteLabel: string;
+
+  goalversePathButton: string;
+  punkiaPathButton: string;
+  proteusPathButton: string;
+  futureAntidotesButton: string;
+
   territoriesEyebrow: string;
   territoriesTitle: string;
+
   bridgeTitle: string;
   bridgeDescription: string;
   bridgeButton: string;
+
   aboutEyebrow: string;
   aboutTitle: string;
   aboutParagraphOne: string;
   aboutParagraphTwo: string;
   aboutParagraphThree: string;
+
   logoBridgeEyebrow: string;
   logoBridgeTitle: string;
   logoBridgeDescription: string;
