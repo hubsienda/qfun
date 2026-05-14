ac3365690e0b8cbf03cfaa15321d720d4c5b6d85
hubsienda <hub@siendaweblines.com>
2026-05-14 17:53:52 +0200

    Update page.tsx

--- app/punkia/page.tsx
+++ app/punkia/page.tsx
@@ -17,7 +17,7 @@ export default function PunkiaPage() {
       title={page.title}
       description={page.description}
       content={page.content}
-      button={page.button}
+      button={page.pageButton}
       showStoreLink={page.showStoreLink === 'true'}
     />
   );
