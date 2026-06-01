a16c75f1ab564279e5b68cd547879542c2783c99
hubsienda <hub@siendaweblines.com>
2026-06-01 14:42:58 +0200

    Update types.ts

--- lib/qoobix/types.ts
+++ lib/qoobix/types.ts
@@ -16,7 +16,7 @@ export type JobStatus =
   | 'failed'
   | 'cancelled';
 
-export type ReportType = 'docx' | 'xlsx' | 'html' | 'csv';
+export type ReportType = 'docx' | 'xlsx' | 'rtf' | 'csv';
 
 export type ClientConfiguration = {
   id: string;
